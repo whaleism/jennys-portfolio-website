96cc7d520f7b64bb70a8f445a9620ed352335553
whaleism <230928187+whaleism@users.noreply.github.com>
2026-05-07 10:28:43 -0500

    Added css styling to Contact section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const menuIcon = document.querySelector("#menu-icon");
 const navLinks = document.querySelector(".nav-links");
 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
 menuIcon.onclick = () => {
   navLinks.classList.toggle("active");
 };
